c62dcb6abcc002329b08d3cecee0d07d233a4cfe
SushiSambo <samknights@gmail.com>
2016-10-30 23:06:17 +0000

    Removed Entity Framework

--- DataImporter/SqlWriterStrategy.cs
+++ DataImporter/SqlWriterStrategy.cs
@@ -1,7 +1,6 @@
 ﻿using DataImporter.interfaces;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity.Core.Objects;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
